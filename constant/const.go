efd555668235876d750842c88a61ce4f1e593bc7
romber <romber2001@163.com>
2020-12-23 16:21:58 +0800

    1. add 2 exit code constant

--- constant/const.go
+++ constant/const.go
@@ -5,20 +5,22 @@ import (
 )
 
 const (
-	ZeroInt                         = 0
-	MinUInt             uint        = 0
-	MaxUInt                         = ^uint(0)
-	MaxInt                          = int(^uint(0) >> 1)
-	MinInt                          = ^MaxInt
-	MinPort                         = 1
-	MaxPort                         = 65535
-	EmptyString                     = ""
-	CurrentDir                      = "./"
-	DefaultRandomString             = "sadfio3mj23gsk9lj8ou"
-	DefaultRandomInt                = 345920654907418
-	TrueString                      = "true"
-	FalseString                     = "false"
-	CRLFString                      = "\n"
-	DefaultFileMode     os.FileMode = 0644
-	DefaultExecFileMode os.FileMode = 0755
+	DefaultNormalExitCode               = 0
+	DefaultAbnormalExitCode             = 1
+	ZeroInt                             = 0
+	MinUInt                 uint        = 0
+	MaxUInt                             = ^uint(0)
+	MaxInt                              = int(^uint(0) >> 1)
+	MinInt                              = ^MaxInt
+	MinPort                             = 1
+	MaxPort                             = 65535
+	EmptyString                         = ""
+	CurrentDir                          = "./"
+	DefaultRandomString                 = "sadfio3mj23gsk9lj8ou"
+	DefaultRandomInt                    = 345920654907418
+	TrueString                          = "true"
+	FalseString                         = "false"
+	CRLFString                          = "\n"
+	DefaultFileMode         os.FileMode = 0644
+	DefaultExecFileMode     os.FileMode = 0755
 )

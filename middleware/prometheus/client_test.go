cd72bad1e3c462b49e9a6f33a4ec42866d981911
romber <romber2001@163.com>
2021-05-14 13:59:09 +0800

    prometheus package: client_test.go: modify some tests

--- middleware/prometheus/client_test.go
+++ middleware/prometheus/client_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/romberli/go-util/constant"
 	"github.com/romberli/log"
 	"github.com/stretchr/testify/assert"
 )
@@ -47,20 +48,20 @@ func TestConn_Execute(t *testing.T) {
 	// }
 
 	// query := "1"
-	// query = `rate(mysql_global_status_queries)[1m]`
+	query = `rate(mysql_global_status_queries)[1m]`
 	// query := `mysql_global_status_queries`
-	// result, err = conn.Execute(query, start, end, step)
-	// asst.Nil(err, "test Execute() failed")
-	// s, err := result.GetString(constant.ZeroInt, constant.ZeroInt)
-	// asst.Nil(err, "test Execute() failed")
-	// ts, err := result.GetString(constant.ZeroInt, 1)
-	// asst.Nil(err, "test Execute() failed")
-	// t.Log(s, ts)
+	result, err = conn.Execute(query, start, end, step)
+	asst.Nil(err, "test Execute() failed")
+	s, err := result.GetString(constant.ZeroInt, constant.ZeroInt)
+	asst.Nil(err, "test Execute() failed")
+	ts, err := result.GetString(constant.ZeroInt, 1)
+	asst.Nil(err, "test Execute() failed")
+	t.Log(s, ts)
 
-	// query = `sum(avg by (node_name,mode) (clamp_max(((avg by (mode,node_name) ( (clamp_max(rate(node_cpu_seconds_total{node_name=~"192-168-137-11",mode!="idle"}[5s]),1)) or (clamp_max(irate(node_cpu_seconds_total{node_name=~"192-168-137-11",mode!="idle"}[5m]),1)) ))*100 or (avg_over_time(node_cpu_average{node_name=~"192-168-137-11", mode!="total", mode!="idle"}[5s]) or avg_over_time(node_cpu_average{node_name=~"192-168-137-11", mode!="total", mode!="idle"}[5m]))),100)))`
-	// result, err = conn.Execute(query, start, end, step)
-	// asst.Nil(err, "test Execute() failed")
-	// t.Log(result)
+	query = `sum(avg by (node_name,mode) (clamp_max(((avg by (mode,node_name) ( (clamp_max(rate(node_cpu_seconds_total{node_name=~"192-168-137-11",mode!="idle"}[5s]),1)) or (clamp_max(irate(node_cpu_seconds_total{node_name=~"192-168-137-11",mode!="idle"}[5m]),1)) ))*100 or (avg_over_time(node_cpu_average{node_name=~"192-168-137-11", mode!="total", mode!="idle"}[5s]) or avg_over_time(node_cpu_average{node_name=~"192-168-137-11", mode!="total", mode!="idle"}[5m]))),100)))`
+	result, err = conn.Execute(query, start, end, step)
+	asst.Nil(err, "test Execute() failed")
+	t.Log(result)
 
 	query = `topk(10, avg by (service_name,schema,table) (sum(mysql_info_schema_table_rows{service_name=~"192-168-10-210:3306"}) by (service_name, schema, table))) > 0`
 	result, err = conn.Execute(query, start, end, step)

51e8f5246bd31dd88a420db9046f2833229ae25c
romber <romber2001@163.com>
2021-05-18 14:46:35 +0800

    prometheus: modify the way of formulating result

--- middleware/prometheus/result.go
+++ middleware/prometheus/result.go
@@ -57,47 +57,44 @@ func NewResult(value model.Value, warnings apiv1.Warnings) *Result {
 	switch v := value.(type) {
 	case *model.Scalar:
 		if v != nil {
-			row := make([]driver.Value, defaultColumnNum)
+			values = make([][]driver.Value, 1)
+			values[constant.ZeroInt] = make([]driver.Value, defaultColumnNum)
 
-			row[0] = float64(v.Value)
-			row[1] = v.Timestamp.Time()
-
-			values = append(values, row)
+			values[constant.ZeroInt][0] = float64(v.Value)
+			values[constant.ZeroInt][1] = v.Timestamp.Time()
 		}
 	case *model.String:
 		if v != nil {
-			row := make([]driver.Value, defaultColumnNum)
+			values = make([][]driver.Value, 1)
+			values[constant.ZeroInt] = make([]driver.Value, defaultColumnNum)
 
-			row[0] = v.Value
-			row[1] = v.Timestamp.Time()
-
-			values = append(values, row)
+			values[constant.ZeroInt][0] = v.Value
+			values[constant.ZeroInt][1] = v.Timestamp.Time()
 		}
 	case model.Vector:
 		if v != nil && v.Len() > constant.ZeroInt {
+			values = make([][]driver.Value, v.Len())
+
 			for i := 0; i < v.Len(); i++ {
-				row := make([]driver.Value, defaultColumnNum)
+				values[i] = make([]driver.Value, defaultColumnNum)
 
-				row[0] = float64(v[i].Value)
-				row[1] = v[i].Timestamp.Time()
-
-				values = append(values, row)
+				values[i][0] = float64(v[i].Value)
+				values[i][1] = v[i].Timestamp.Time()
 			}
 		}
 	case model.Matrix:
+		// note that only the first matrix value will be processed,
+		// if a query returns more than one matrix,
+		// use GetRaw() function to get the raw data which is returned by prometheus go client package
 		if v != nil && v.Len() > constant.ZeroInt {
-			// note that only the first matrix value will be processed,
-			// if a query returns more than one matrix,
-			// use GetRaw() function to get the raw data which is returned by prometheus go client package
 			samplePairs := v[constant.ZeroInt].Values
+			values = make([][]driver.Value, len(samplePairs))
 
 			for i := 0; i < len(samplePairs); i++ {
-				row := make([]driver.Value, defaultColumnNum)
+				values[i] = make([]driver.Value, defaultColumnNum)
 
-				row[0] = float64(samplePairs[i].Value)
-				row[1] = samplePairs[i].Timestamp.Time()
-
-				values = append(values, row)
+				values[i][0] = float64(samplePairs[i].Value)
+				values[i][1] = samplePairs[i].Timestamp.Time()
 			}
 		}
 	}

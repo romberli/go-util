e1616b5837ee85c7fe5fe68744452e97691e7bc3
Romber Li <romber2001@163.com>
2021-08-20 17:56:55 +0800

    linux package: add MatchMountPoint() function

--- linux/mount.go
+++ linux/mount.go
@@ -1,6 +1,8 @@
 package linux
 
 import (
+	"errors"
+	"fmt"
 	"os"
 	"path/filepath"
 	"syscall"
@@ -36,4 +38,23 @@ func FindMountPoint(path string) (string, error) {
 	}
 
 	return path, nil
+}
+
+// MatchMountPoint matches mount point of given path in the mount point slice,
+// if nothing matched, it returns "/" as default mount point
+func MatchMountPoint(path string, mountPoints []string) (string, error) {
+	if !filepath.IsAbs(path) {
+		return constant.EmptyString, errors.New(fmt.Sprintf("path must be an absolute path, %s is not valid", path))
+	}
+	if path == constant.RootDir {
+		return constant.RootDir, nil
+	}
+
+	for _, mountPoint := range mountPoints {
+		if mountPoint == path {
+			return mountPoint, nil
+		}
+	}
+
+	return MatchMountPoint(filepath.Dir(path), mountPoints)
 }
--- linux/mount_test.go
+++ linux/mount_test.go
@@ -3,16 +3,27 @@ package linux
 import (
 	"testing"
 
+	"github.com/romberli/go-util/constant"
 	"github.com/stretchr/testify/assert"
 )
 
 const (
 	defaultPath       = "/boot/grub"
 	defaultMountPoint = "/boot"
+
+	// mount point
+	data   = "/data"
+	mysql  = "/data/mysql"
+	binlog = "/data/mysql/binlog"
+	other  = "/other/dir"
+	// path
+	mysqlDataDir = "/data/mysql/data"
+	someOtherDir = "/some/other/dir"
 )
 
 func TestAll(t *testing.T) {
 	TestFindMountPoint(t)
+	TestMatchMountPoint(t)
 }
 
 func TestFindMountPoint(t *testing.T) {
@@ -22,4 +33,18 @@ func TestFindMountPoint(t *testing.T) {
 	asst.Nil(err, "test FindMountPoint() failed")
 	asst.Equal(defaultMountPoint, mountPoint, "test FindMountPoint() failed")
 	t.Logf("path: %s, mount point: %s", defaultPath, mountPoint)
+}
+
+func TestMatchMountPoint(t *testing.T) {
+	asst := assert.New(t)
+
+	mountPoints := []string{constant.RootDir, data, mysql, binlog, other}
+
+	mountPoint, err := MatchMountPoint(mysqlDataDir, mountPoints)
+	asst.Nil(err, "test MatchMountPoint() failed")
+	asst.Equal(mysql, mountPoint, "test MatchMountPoint() failed")
+
+	mountPoint, err = MatchMountPoint(someOtherDir, mountPoints)
+	asst.Nil(err, "test MatchMountPoint() failed")
+	asst.Equal(constant.RootDir, mountPoint, "test MatchMountPoint() failed")
 }

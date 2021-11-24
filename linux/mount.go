770f78d00c0b38202b47b342443540ef9f426142
Romber Li <romber2001@163.com>
2021-11-24 16:24:29 +0800

    linux: use common.IsAbs() instead of filepath.IsAbs

--- linux/mount.go
+++ linux/mount.go
@@ -5,13 +5,14 @@ import (
 	"fmt"
 	"path/filepath"
 
+	"github.com/romberli/go-util/common"
 	"github.com/romberli/go-util/constant"
 )
 
 // MatchMountPoint matches mount point of given path in the mount point slice,
 // if nothing matched, it returns "/" as default mount point
 func MatchMountPoint(path string, mountPoints []string) (string, error) {
-	if !filepath.IsAbs(path) {
+	if !common.IsAbs(path) {
 		return constant.EmptyString, errors.New(fmt.Sprintf("path must be an absolute path, %s is not valid", path))
 	}
 	if path == constant.RootDir {

45eceaf725a1ba17d8e8c5f3094fe105d40fe474
Romber Li <romber2001@163.com>
2022-06-30 15:25:25 +0800

    middleware: add message to rabbitmq package

---
+++ middleware/rabbitmq/message.go
@@ -0,0 +1,224 @@
+package rabbitmq
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/pingcap/errors"
+	"github.com/romberli/go-util/constant"
+)
+
+const (
+	SQLTypeInsert = "INSERT"
+	SQLTypeUpdate = "UPDATE"
+	SQLTypeDelete = "DELETE"
+
+	andString = "AND"
+)
+
+type Message struct {
+	SQLType   string                   `json:"type"`
+	IsDDL     bool                     `json:"isDdl"`
+	DBName    string                   `json:"database"`
+	TableName string                   `json:"table"`
+	PKNames   []string                 `json:"pkNames"`
+	Columns   map[string]string        `json:"mysqlType"`
+	Data      []map[string]interface{} `json:"data"`
+	Old       []map[string]interface{} `json:"old"`
+}
+
+// NewMessage returns a new *Message
+func NewMessage(sqlType string, isDDL bool, dbName string, tableName string, pkNames []string,
+	columns map[string]string, data, old []map[string]interface{}) *Message {
+	return newMessage(sqlType, isDDL, dbName, tableName, pkNames, columns, data, old)
+}
+
+// newMessage returns a new *Message
+func newMessage(sqlType string, isDDL bool, dbName string, tableName string, pkNames []string,
+	columns map[string]string, data, old []map[string]interface{}) *Message {
+	return &Message{
+		SQLType:   sqlType,
+		IsDDL:     isDDL,
+		DBName:    dbName,
+		TableName: tableName,
+		PKNames:   pkNames,
+		Columns:   columns,
+		Data:      data,
+		Old:       old,
+	}
+}
+
+// NewEmptyMessage returns a new empty *Message
+func NewEmptyMessage() *Message {
+	return &Message{}
+}
+
+// GetSQLType returns the SQLType
+func (m *Message) GetSQLType() string {
+	return m.SQLType
+}
+
+// GetIsDDL returns the IsDDL
+func (m *Message) GetIsDDL() bool {
+	return m.IsDDL
+}
+
+// GetDBName returns the DBName
+func (m *Message) GetDBName() string {
+	return m.DBName
+}
+
+// GetTableName returns the TableName
+func (m *Message) GetTableName() string {
+	return m.TableName
+
+}
+
+// GetPKNames returns the PKNames
+func (m *Message) GetPKNames() []string {
+	return m.PKNames
+}
+
+// GetColumns returns the Columns
+func (m *Message) GetColumns() map[string]string {
+	return m.Columns
+}
+
+// GetData returns the Data
+func (m *Message) GetData() []map[string]interface{} {
+	return m.Data
+}
+
+// GetOld returns the Old
+func (m *Message) GetOld() []map[string]interface{} {
+	return m.Old
+}
+
+// GetColumnNames gets the column names
+func (m *Message) GetColumnNames() []string {
+	var columnNames []string
+	for name := range m.GetColumns() {
+		columnNames = append(columnNames, name)
+	}
+
+	return columnNames
+}
+
+// ConvertToSQL returns a map of the sql statement and the values
+func (m *Message) ConvertToSQL(ignoreDDL bool) ([]map[string][]interface{}, error) {
+	if m.GetIsDDL() {
+		if ignoreDDL {
+			return nil, nil
+		}
+
+		return nil, errors.New("does not support ddl statement, either ignore this or avoid to convert ddl statement")
+	}
+
+	switch m.GetSQLType() {
+	case SQLTypeInsert:
+		return m.convertToInsertSQL()
+	case SQLTypeUpdate:
+		return m.convertToUpdateSQL()
+	case SQLTypeDelete:
+		return m.convertToDeleteSQL()
+	default:
+		return nil, errors.Errorf("sql type must be one of [INSERT, UPDATE, DELETE], %s is not supported", m.GetSQLType())
+	}
+}
+
+// convertToInsertSQL converts message to a insert sql statement
+func (m *Message) convertToInsertSQL() ([]map[string][]interface{}, error) {
+	if len(m.GetData()) == constant.ZeroInt {
+		return nil, errors.New("data should not be empty")
+	}
+
+	var (
+		columnNamesStr string
+		valuesStr      string
+		values         []interface{}
+	)
+
+	// use column slice to determine the column order
+	columnNames := m.GetColumnNames()
+
+	for _, columnName := range columnNames {
+		columnNamesStr += columnName + constant.CommaString
+	}
+
+	columnNamesStr = strings.TrimSuffix(columnNamesStr, constant.CommaString)
+
+	value := constant.LeftParenthesisString
+	value += strings.Repeat(constant.QuestionMarkString+constant.CommaString, len(columnNames))
+	value = strings.TrimSuffix(value, constant.CommaString)
+	value += constant.RightParenthesisString + constant.CommaString
+	valuesStr = strings.Repeat(value, len(m.GetData()))
+	valuesStr = strings.TrimSuffix(valuesStr, constant.CommaString)
+
+	sql := `INSERT INTO %s.%s(%s) VALUES %s ;`
+	sql = fmt.Sprintf(sql, m.GetDBName(), m.GetTableName(), columnNamesStr, valuesStr)
+
+	for _, data := range m.GetData() {
+		for _, columnName := range columnNames {
+			values = append(values, data[columnName])
+		}
+	}
+
+	return []map[string][]interface{}{{sql: values}}, nil
+}
+
+// convertToUpdateSQL converts message to a update sql statement
+func (m *Message) convertToUpdateSQL() ([]map[string][]interface{}, error) {
+	if len(m.GetData()) == constant.ZeroInt {
+		return nil, errors.New("data should not be empty")
+	}
+
+	lenData := len(m.GetData())
+	lenOld := len(m.GetOld())
+	if len(m.GetData()) != len(m.GetOld()) {
+		return nil, errors.Errorf("the lengths of the data and old are not the same. table: %s, data: %d, old: %d", m.GetTableName(), lenData, lenOld)
+	}
+
+	if len(m.GetPKNames()) == constant.ZeroInt {
+		return nil, errors.Errorf("table does not have a primary key. table: %s", m.GetTableName())
+	}
+
+	statements := make([]map[string][]interface{}, lenData)
+
+	for i, old := range m.GetOld() {
+		var values []interface{}
+
+		sql := `UPDATE %s.%s SET %s WHERE%s;`
+		setStr := constant.EmptyString
+		whereStr := constant.EmptyString
+
+		data := m.GetData()[i]
+		for columnName, _ := range old {
+			values = append(values, data[columnName])
+			setStr += columnName + constant.EqualString + constant.QuestionMarkString + constant.CommaString
+		}
+
+		setStr = strings.TrimSuffix(setStr, constant.CommaString)
+
+		for _, pkName := range m.GetPKNames() {
+			pkValue, ok := old[pkName]
+			if !ok {
+				pkValue = data[pkName]
+			}
+
+			values = append(values, pkValue)
+			whereStr += fmt.Sprintf(" %s=? AND", pkName)
+		}
+
+		whereStr = strings.TrimSuffix(whereStr, andString)
+		sql = fmt.Sprintf(sql, m.GetDBName(), m.GetTableName(), setStr, whereStr)
+
+		statements[i] = map[string][]interface{}{sql: values}
+	}
+
+	return statements, nil
+}
+
+func (m *Message) convertToDeleteSQL() ([]map[string][]interface{}, error) {
+	// todo: implement
+	return nil, errors.New("does not support delete statement")
+}
---
+++ middleware/rabbitmq/message_test.go
@@ -0,0 +1,246 @@
+package rabbitmq
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/romberli/go-util/common"
+	"github.com/stretchr/testify/assert"
+)
+
+const (
+	testMessageIsDDL            = false
+	testMessageDBName           = "test_db"
+	testMessageTableName        = "test_table"
+	testMessageInsertJSONString = `
+        {
+            "data": [
+                {
+                    "pk1": 1,
+                    "pk2": "a",
+                    "col1": "test_new_col1_a",
+                    "col2": "test_new_col2_a"
+                },
+                {
+                    "pk1": 2,
+                    "pk2": "b",
+                    "col1": "test_new_col1_b",
+                    "col2": "test_new_col2_b"
+                }
+            ],
+            "database": "test_db",
+            "es": 1656315780000,
+            "id": 1383,
+            "isDdl": false,
+            "mysqlType": {
+                "pk1": "int",
+                "pk2": "varchar(100)",
+                "col1": "varchar(100)",
+                "col2": "varchar(100)"
+            },
+            "old": null,
+            "pkNames": [
+                "pk1",
+                "pk2"
+            ],
+            "sql": "",
+            "sqlType": {
+                "pk1": 12,
+                "pk2": 12,
+                "col1": 12,
+                "col2": 12
+            },
+            "table": "test_table",
+            "ts": 1656315780000,
+            "type": "INSERT"
+        }
+    `
+	testMessageUpdateJSONString = `
+        {
+            "data": [
+                {
+                    "pk1": 1,
+                    "pk2": "a",
+                    "col1": "test_update_col1_a",
+                    "col2": "test_update_col2_a"
+                },
+                {
+                    "pk1": 2,
+                    "pk2": "b",
+                    "col1": "test_update_col1_b",
+                    "col2": "test_update_col2_b"
+                }
+            ],
+            "database": "test_db",
+            "es": 1656315780000,
+            "id": 1383,
+            "isDdl": false,
+            "mysqlType": {
+                "pk1": "int",
+                "pk2": "varchar(100)",
+                "col1": "varchar(100)",
+                "col2": "varchar(100)"
+            },
+            "old": [
+                {
+                    "col1": "test_old_col1_a"
+                },
+                {
+                    "col2": "test_old_col2_b"
+                }
+            ],
+            "pkNames": [
+                "pk1",
+                "pk2"
+            ],
+            "sql": "",
+            "sqlType": {
+                "pk1": 12,
+                "pk2": 12,
+                "col1": 12,
+                "col2": 12
+            },
+            "table": "test_table",
+            "ts": 1656315780000,
+            "type": "UPDATE"
+        }
+    `
+	testMessageDeleteJSONString = `
+        {
+            "data": null,
+            "database": "test_db",
+            "es": 1656315780000,
+            "id": 1383,
+            "isDdl": false,
+            "mysqlType": {
+                "pk1": "int",
+                "pk2": "varchar(100)",
+                "col1": "varchar(100)",
+                "col2": "varchar(100)"
+            },
+            "old": [
+                {
+                    "pk1": 1,
+                    "pk2": "a",
+                    "col1": "test_old_col1_a",
+                    "col2": "test_old_col2_a"
+                },
+                {
+                    "pk1": 2,
+                    "pk2": "b",
+                    "col1": "test_old_col1_b",
+                    "col2": "test_old_col2_b"
+                }
+            ],
+            "pkNames": [
+                "pk1",
+                "pk2"
+            ],
+            "sql": "",
+            "sqlType": {
+                "pk1": 12,
+                "pk2": 12,
+                "col1": 12,
+                "col2": 12
+            },
+            "table": "test_table",
+            "ts": 1656315780000,
+            "type": "DELETE"
+        }
+    `
+	testMessageInsertSQL = `INSERT INTO test_db.test_table(pk1,pk2,col1,col2) VALUES (?,?,?,?),(?,?,?,?) ;`
+	testMessageUpdateSQL = `UPDATE test_db.test_table SET col1=? WHERE pk1=? AND pk2=? ;`
+)
+
+var (
+	testMessagePKNames = []string{"pk1", "pk2"}
+	testMessageColumns = map[string]string{"pk1": "int", "pk2": "varchar(100)", "col1": "varchar(100)", "col2": "varchar(100)"}
+	testMessageNewData = []map[string]interface{}{
+		map[string]interface{}{"pk1": 1, "pk2": "a", "col1": "test_new_col1_a", "col2": "test_new_col2_a"},
+		map[string]interface{}{"pk1": 2, "pk2": "b", "col1": "test_new_col1_b", "col2": "test_new_col2_b"},
+	}
+	testMessageUpdateData = []map[string]interface{}{
+		map[string]interface{}{"pk1": 1, "pk2": "a", "col1": "test_update_col1_a", "col2": "test_update_col2_a"},
+		map[string]interface{}{"pk1": 2, "pk2": "b", "col1": "test_update_col1_b", "col2": "test_update_col2_b"},
+	}
+	testMessageOld = []map[string]interface{}{
+		map[string]interface{}{"col1": "test_old_col1_a"},
+		map[string]interface{}{"col1": "test_old_col2_b"},
+	}
+)
+
+func testMessageCreateMessage(sqlType string, data, old []map[string]interface{}) *Message {
+	return NewMessage(sqlType, testMessageIsDDL, testMessageDBName, testMessageTableName, testMessagePKNames, testMessageColumns, data, old)
+}
+
+func TestMessage_All(t *testing.T) {
+	TestMessage_GetColumnNames(t)
+	TestMessage_ConvertToSQL(t)
+}
+
+func TestMessage_GetColumnNames(t *testing.T) {
+	asst := assert.New(t)
+
+	testMessage := NewEmptyMessage()
+	err := json.Unmarshal([]byte(testMessageInsertJSONString), &testMessage)
+	asst.Nil(err, common.CombineMessageWithError("test GetColumnNames() failed", err))
+
+	columnNames := testMessage.GetColumnNames()
+	asst.Equal(len(testMessageColumns), len(columnNames), "test GetColumnNames() failed")
+	for _, columnName := range columnNames {
+		asst.Contains(testMessageColumns, columnName, "test GetColumnNames() failed")
+	}
+}
+
+func TestMessage_ConvertToSQL(t *testing.T) {
+	TestMessage_convertToInsertSQL(t)
+	TestMessage_convertToUpdateSQL(t)
+}
+
+func TestMessage_convertToInsertSQL(t *testing.T) {
+	asst := assert.New(t)
+
+	testMessage := NewEmptyMessage()
+	err := json.Unmarshal([]byte(testMessageInsertJSONString), &testMessage)
+	asst.Nil(err, common.CombineMessageWithError("test convertToInsertSQL() failed", err))
+
+	statements, err := testMessage.ConvertToSQL(true)
+	asst.Nil(err, common.CombineMessageWithError("test ConvertToSQL() failed", err))
+	asst.Equal(1, len(statements), "test ConvertToSQL() failed")
+	for _, statement := range statements {
+		for _, values := range statement {
+			asst.Equal(8, len(values), "test ConvertToSQL() failed")
+		}
+	}
+	t.Log(testMessageInsertSQL)
+	for _, statement := range statements {
+		for sql, values := range statement {
+			t.Log(sql)
+			t.Log(values)
+		}
+	}
+}
+
+func TestMessage_convertToUpdateSQL(t *testing.T) {
+	asst := assert.New(t)
+
+	testMessage := NewEmptyMessage()
+	err := json.Unmarshal([]byte(testMessageUpdateJSONString), &testMessage)
+	asst.Nil(err, common.CombineMessageWithError("test convertToUpdateSQL() failed", err))
+
+	statements, err := testMessage.ConvertToSQL(true)
+	asst.Nil(err, common.CombineMessageWithError("test ConvertToSQL() failed", err))
+	asst.Equal(2, len(statements), "test ConvertToSQL() failed")
+	for _, statement := range statements {
+		for _, values := range statement {
+			asst.Equal(3, len(values), "test ConvertToSQL() failed")
+		}
+	}
+	t.Log(testMessageUpdateSQL)
+	for _, statement := range statements {
+		for sql, values := range statement {
+			t.Log(sql)
+			t.Log(values)
+		}
+	}
+}

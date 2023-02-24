0cd305360aff92a307b99f6e6a746cefd409b7ce
Romber Li <romber2001@163.com>
2023-02-24 19:15:59 +0800

    linux: add IsValidIP() and CompareIP() functions

--- linux/network.go
+++ linux/network.go
@@ -2,6 +2,8 @@ package linux
 
 import (
 	"net"
+	"strconv"
+	"strings"
 
 	"github.com/pingcap/errors"
 	"github.com/romberli/go-util/constant"
@@ -53,4 +55,45 @@ func getAddrDefaultIP(addr net.Addr) string {
 	}
 
 	return ip.String()
+}
+
+// IsValidIP checks if the ip is valid
+func IsValidIP(ip string) bool {
+	return net.ParseIP(ip) != nil
+}
+
+// CompareIP compares two ip addresses,
+// if ip1 is equal to ip2, it returns 0
+// if ip1 is less than ip2, it returns -1
+// if ip1 is greater than ip2, it returns 1
+func CompareIP(ip1, ip2 string) (int, error) {
+	if !IsValidIP(ip1) {
+		return constant.ZeroInt, errors.Errorf("ip addr must be formatted as A.B.C.D, %s is not valid", ip1)
+	}
+	if !IsValidIP(ip2) {
+		return constant.ZeroInt, errors.Errorf("ip addr must be formatted as A.B.C.D, %s is not valid", ip2)
+	}
+
+	ipList1 := strings.Split(ip1, constant.DotString)
+	ipList2 := strings.Split(ip2, constant.DotString)
+
+	for i := constant.ZeroInt; i < len(ipList1); i++ {
+		p1, err := strconv.Atoi(ipList1[i])
+		if err != nil {
+			return constant.ZeroInt, errors.Trace(err)
+		}
+		p2, err := strconv.Atoi(ipList2[i])
+		if err != nil {
+			return constant.ZeroInt, errors.Trace(err)
+		}
+
+		if p1 < p2 {
+			return -1, nil
+		}
+		if p1 > p2 {
+			return 1, nil
+		}
+	}
+
+	return constant.ZeroInt, nil
 }

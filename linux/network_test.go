0fbb44624c5e7daed2fa777a504fac9209525687
Romber Li <romber2001@163.com>
2023-02-27 09:55:21 +0800

    linux: add IsValidIP() and CompareIP() functions

--- linux/network_test.go
+++ linux/network_test.go
@@ -6,10 +6,36 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestIsValidHost(t *testing.T) {
+const (
+	testValidIP1  = "127.0.0.1"
+	testValidIP2  = "192.168.137.11"
+	testInvalidIP = "127.0.0.256"
+)
+
+func TestNetwork_All(t *testing.T) {
+	TestNetwork_GetDefaultIP(t)
+
+}
+
+func TestNetwork_GetDefaultIP(t *testing.T) {
 	asst := assert.New(t)
 
 	hostIP, err := GetDefaultIP()
 	t.Logf("host ip: %s", hostIP)
 	asst.Nil(err, "test GetDefaultIP failed")
+}
+
+func TestNetwork_IsValidIP(t *testing.T) {
+	asst := assert.New(t)
+
+	asst.True(IsValidIP(testValidIP1), "test IsValidIP() failed")
+	asst.False(IsValidIP(testInvalidIP), "test IsValidIP() failed")
+}
+
+func TestNetwork_CompareIP(t *testing.T) {
+	asst := assert.New(t)
+
+	result, err := CompareIP(testValidIP1, testValidIP2)
+	asst.Nil(err, "test CompareIP() failed")
+	asst.Equal(-1, result, "test CompareIP() failed")
 }

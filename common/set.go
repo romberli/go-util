03f75b502e870b23f84acbaecfd89fffd5d34c41
Romber Li <romber2001@163.com>
2023-09-12 18:03:19 +0800

    common: add calculation of slice

--- common/set.go
+++ common/set.go
@@ -2,6 +2,36 @@ package common
 
 import "github.com/romberli/go-util/types"
 
+// UnionSlice returns the union of two slices
+func UnionSlice[T types.Primitive](first, second []T) []T {
+	var result []T
+
+	for _, v := range first {
+		result = append(result, v)
+	}
+
+	for _, v := range second {
+		if !ElementInSlice(result, v) {
+			result = append(result, v)
+		}
+	}
+
+	return result
+}
+
+// IntersectSlice returns the intersection of two slices
+func IntersectSlice[T types.Primitive](first, second []T) []T {
+	var result []T
+
+	for _, v := range first {
+		if ElementInSlice(second, v) {
+			result = append(result, v)
+		}
+	}
+
+	return result
+}
+
 // SubtractSlice returns all the elements in first but not in second
 func SubtractSlice[T types.Primitive](first, second []T) []T {
 	var result []T

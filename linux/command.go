b9adc79e43f2430d26c8bd585fbdecada6bf2d38
Romber Li <romber2001@163.com>
2024-09-06 11:38:44 +0800

    linux: modify ExecuteCommand() function, now it's able to add options to the command.

--- linux/command.go
+++ linux/command.go
@@ -7,37 +7,92 @@ import (
 
 	"github.com/pingcap/errors"
 
+	"github.com/romberli/go-util/common"
 	"github.com/romberli/go-util/constant"
 )
 
+const (
+	shCommand   = "sh"
+	shPath      = "/bin/sh"
+	bashCommand = "/bin/bash"
+	dashCArg    = "-c"
+)
+
+type CommandOption interface {
+	apply(*exec.Cmd)
+}
+
+type optionFunc func(*exec.Cmd)
+
+func (f optionFunc) apply(cmd *exec.Cmd) {
+	f(cmd)
+}
+
+func WorkDirOption(workDir string) CommandOption {
+	return optionFunc(func(cmd *exec.Cmd) {
+		cmd.Dir = workDir
+	})
+}
+
+func UseSHCOption() CommandOption {
+	return optionFunc(func(cmd *exec.Cmd) {
+		cmd.Path = shPath
+		cmd.Args = []string{shCommand, dashCArg, common.ConvertSliceToString(cmd.Args, constant.SpaceString)}
+	})
+}
+
 // ExecuteCommand is an alias of ExecuteCommandAndWait
-func ExecuteCommand(command string) (output string, err error) {
-	return ExecuteCommandAndWait(command)
+// 1. first arg must be the working directory
+func ExecuteCommand(command string, options ...CommandOption) (output string, err error) {
+	return ExecuteCommandAndWait(command, options...)
 }
 
 // ExecuteCommandAndWait executes shell command and wait for it to complete
-func ExecuteCommandAndWait(command string) (output string, err error) {
-	var stdoutBuffer bytes.Buffer
-
-	c := strings.Split(command, constant.SpaceString)
-	cmd := exec.Command(c[constant.ZeroInt], c[constant.OneInt:]...)
-	cmd.Stdout = &stdoutBuffer
-	cmd.Stderr = &stdoutBuffer
+func ExecuteCommandAndWait(command string, options ...CommandOption) (output string, err error) {
+	cmd, err := getCommand(command, options...)
+	if err != nil {
+		return constant.EmptyString, err
+	}
 
 	err = cmd.Run()
 
-	return stdoutBuffer.String(), errors.Trace(err)
+	return cmd.Stdout.(*bytes.Buffer).String(), errors.Trace(err)
 }
 
 // ExecuteCommandNoWait executes shell command and does not wait for it to complete
-func ExecuteCommandNoWait(command string) (output string, err error) {
-	var stdoutBuffer bytes.Buffer
-
-	cmd := exec.Command("sh", "-c", command)
-	cmd.Stdout = &stdoutBuffer
-	cmd.Stderr = &stdoutBuffer
+func ExecuteCommandNoWait(command string, options ...CommandOption) (output string, err error) {
+	cmd, err := getCommand(command, options...)
+	if err != nil {
+		return constant.EmptyString, err
+	}
 
 	err = cmd.Start()
 
-	return stdoutBuffer.String(), errors.Trace(err)
+	return cmd.Stdout.(*bytes.Buffer).String(), errors.Trace(err)
+}
+
+func getCommand(command string, options ...CommandOption) (*exec.Cmd, error) {
+	var (
+		stdoutBuffer bytes.Buffer
+	)
+
+	commandList := strings.Split(command, constant.SpaceString)
+	if len(commandList) == constant.ZeroInt {
+		return nil, errors.New("command is empty")
+	}
+
+	cmdName := commandList[constant.ZeroInt]
+	if len(commandList) > constant.TwoInt && (cmdName == shCommand || cmdName == bashCommand) && commandList[constant.OneInt] == dashCArg {
+		commandList = strings.SplitN(command, constant.SpaceString, constant.ThreeInt)
+	}
+
+	cmd := exec.Command(commandList[constant.ZeroInt], commandList[constant.OneInt:]...)
+	cmd.Stdout = &stdoutBuffer
+	cmd.Stderr = &stdoutBuffer
+
+	for _, option := range options {
+		option.apply(cmd)
+	}
+
+	return cmd, nil
 }
--- linux/command_test.go
+++ linux/command_test.go
@@ -9,24 +9,30 @@ import (
 
 func TestCommand(t *testing.T) {
 	var (
-		err    error
-		cmd    string
-		stdout string
+		err     error
+		workDir string
+		cmd     string
+		stdout  string
 	)
 
 	asst := assert.New(t)
 
+	workDir = "/Users/romber/source_code/go/src/github.com/romberli/go-util"
 	cmd = "ls -l /tmp"
+	cmd = `go list -m -f '{{if not .Indirect}}{{.Path}}@{{.Version}}{{end}}' all`
+	// cmd = `cd /Users/romber/source_code/go/src/github.com/romberli/go-util && go list -m -f '{{if not .Indirect}}{{.Path}}@{{.Version}}{{end}}' all`
+	// cmd = `sh -c cd /Users/romber/source_code/go/src/github.com/romberli/go-util && go list -m -f '{{if not .Indirect}}{{.Path}}@{{.Version}}{{end}}' all`
+	// cmd = "ls -l /tmp"
 
 	// test command
 	t.Log("==========test command started.==========")
-	stdout, err = ExecuteCommand(cmd)
+	stdout, err = ExecuteCommand(cmd, WorkDirOption(workDir), UseSHCOption())
 	t.Log(fmt.Sprintf("stdout: %s", stdout))
 	asst.Nil(err, "test command failed.\ncmd: %s\n%v", cmd, err)
 
-	cmd = "ls -l /tmp/1234"
-	stdout, err = ExecuteCommand(cmd)
-	t.Log(fmt.Sprintf("stdout: %s", stdout))
-	asst.NotNil(err, "test command failed.\ncmd: %s\n%v", cmd, err)
-	t.Log("==========test command completed.==========\n")
+	// cmd = "ls -l /tmp/1234"
+	// stdout, err = ExecuteCommand(cmd)
+	// t.Log(fmt.Sprintf("stdout: %s", stdout))
+	// asst.NotNil(err, "test command failed.\ncmd: %s\n%v", cmd, err)
+	// t.Log("==========test command completed.==========\n")
 }
